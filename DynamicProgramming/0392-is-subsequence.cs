430cbbf3363332c1db89fb608b3099cf75cc0598
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2021-10-30 17:34:52 +0200

    Create 0392-is-subsequence.cs

---
+++ DynamicProgramming/0392-is-subsequence.cs
@@ -0,0 +1,28 @@
+
+// 392. Is Subsequence
+// https://leetcode.com/problems/is-subsequence/
+// Difficulty: Easy
+
+public class Solution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        int i = 0;
+        int j = 0;
+        while ((i < s.Length) && (j < t.Length))
+        {
+            if (s[i] == t[j])
+            {
+                i += 1;
+            }
+            j += 1;
+
+        }
+        if (i == s.Length)
+        {
+            return true;
+        }
+        else return false;
+    }
+}
+        
