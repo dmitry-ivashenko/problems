f503849f8f39f42e8f44fd8e4353179574e11e8c
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2021-12-10 23:28:42 +0100

    Create 0763-partition-labels.cs

---
+++ Greedy/0763-partition-labels.cs
@@ -0,0 +1,35 @@
+
+// 763. Partition Labels
+// https://leetcode.com/problems/partition-labels/
+// Difficulty: Medium
+
+public class Solution
+{
+    public IList<int> PartitionLabels(string s)
+    {
+
+        var chars = new int[26];
+        for (var ch = 0; ch < s.Length; ch++)
+        {
+            chars[s[ch] - 'a'] = ch;
+        }
+
+        var result = new List<int>();
+        var end = 0;
+        var size = 0;
+        for (var i = 0; i < s.Length; i++)
+        {
+            end = Math.Max(chars[s[i] - 'a'], end);
+            size++;
+            if (i == end)
+            {
+                result.Add(size);
+                size = 0;
+            }
+
+        }
+
+        return result;
+    }
+}
+        
