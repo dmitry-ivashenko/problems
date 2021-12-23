23bc17e842f228803a3716378f7003cdac7bd0ef
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2021-12-23 10:26:48 +0100

    Create 0136-single-number.cs

---
+++ BitManipulation/0136-single-number.cs
@@ -0,0 +1,16 @@
+
+// 136. Single Number
+// https://leetcode.com/problems/single-number/
+// Difficulty: Easy
+
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        var x = 0;
+        for (int i=0;i < nums.Length; i++)
+        {
+            x ^= nums[i];
+        }
+        return x;
+    }
+}
+        
