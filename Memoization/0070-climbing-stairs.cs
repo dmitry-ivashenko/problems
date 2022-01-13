a46ac7dcf30d8b1444f3c722a5ab0a239d9a27cf
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2022-01-13 05:25:25 +0100

    Create 0070-climbing-stairs.cs

---
+++ Memoization/0070-climbing-stairs.cs
@@ -0,0 +1,23 @@
+
+// 70. Climbing Stairs
+// https://leetcode.com/problems/climbing-stairs/
+// Difficulty: Easy
+
+public class Solution {
+    public int ClimbStairs(int n) {
+        
+        int one = 1;
+        int two = 1;
+        
+        for(int i = 0; i < n - 1; i++)
+        {
+            int temp = one;
+            one = one + two;
+            two = temp;
+        }
+        
+        return one;
+    }
+}
+
+        
