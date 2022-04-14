dd8490da3bbdf372f9b5e5c5ac67dc94e54625c9
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2022-04-14 16:55:15 +0200

    Create 0190-reverse-bits.cs

---
+++ BitManipulation/0190-reverse-bits.cs
@@ -0,0 +1,21 @@
+
+// 190. Reverse Bits
+// https://leetcode.com/problems/reverse-bits/
+// Difficulty: Easy
+
+public class Solution 
+{
+    public uint reverseBits(uint n) 
+    {
+		uint result = 0;
+		for (int i = 0; i < 32; i++)
+		{
+			uint temp = (n & 1);
+			result = (result << 1) + temp;
+			n >>= 1;
+		}
+        return result;
+    } 
+}
+
+        
