eafcaaf6f05f12c283c452c66eb265a295afdf57
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2022-07-02 23:33:11 +0200

    Create 0169-majority-element.cs

---
+++ Counting/0169-majority-element.cs
@@ -0,0 +1,20 @@
+
+// 169. Majority Element
+// https://leetcode.com/problems/majority-element/
+// Difficulty: Easy
+
+public class Solution {
+    public int MajorityElement(int[] nums) {
+        int res = 0;
+        int count = 0;
+
+        for(var i = 0; i < nums.Length; i ++){
+            if (count == 0){
+                res = nums[i];
+            }
+            count += (nums[i] == res) ? 1 : -1;
+        }
+        return res;
+    }
+}
+        
