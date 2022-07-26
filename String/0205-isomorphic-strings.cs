6df8b89b45f350f4f3609f9fb9db3d9b2da52faf
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2022-07-26 17:40:53 +0200

    Create 0205-isomorphic-strings.cs

---
+++ String/0205-isomorphic-strings.cs
@@ -0,0 +1,35 @@
+
+// 205. Isomorphic Strings
+// https://leetcode.com/problems/isomorphic-strings/
+// Difficulty: Easy
+
+public class Solution {
+    public bool IsIsomorphic(string s, string t) {
+        Dictionary<string, string> mapST = new Dictionary<string, string>();
+        Dictionary<string, string> mapTS = new Dictionary<string, string>();
+
+        for (int i = 0; i < s.Length; i++) {
+            string sChar = s[i].ToString();
+            string tChar = t[i].ToString();
+
+            if (mapST.ContainsKey(sChar)) {
+                if (mapST[sChar] != tChar) {
+                    return false;
+                }
+            } else {
+                mapST.Add(sChar, tChar);
+            }
+
+            if (mapTS.ContainsKey(tChar)) {
+                if (mapTS[tChar] != sChar) {
+                    return false;
+                }
+            } else {
+                mapTS.Add(tChar, sChar);
+            }
+        }
+
+        return true;
+    }
+}
+        
