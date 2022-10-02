eda7bed895554f08443537b0ffedc9944006eee0
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2022-10-02 10:59:22 +0200

    Create 0929-unique-email-addresses.cs

---
+++ String/0929-unique-email-addresses.cs
@@ -0,0 +1,15 @@
+
+// 929. Unique Email Addresses
+// https://leetcode.com/problems/unique-email-addresses/
+// Difficulty: Easy
+
+public class Solution {
+    public int NumUniqueEmails(string[] emails) 
+	=> emails.Select(x => x.Split('@')).
+              Select(split => split[0].
+              Split('+')[0].
+              Replace(".", "") + "@" + split[1]).
+              Distinct().
+              Count();
+}
+        
