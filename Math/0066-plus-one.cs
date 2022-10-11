f55d7056b0214458584af38b5f67925ef3269570
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2022-10-12 00:37:47 +0200

    Create 0066-plus-one.cs

---
+++ Math/0066-plus-one.cs
@@ -0,0 +1,18 @@
+
+// 66. Plus One
+// https://leetcode.com/problems/plus-one/
+// Difficulty: Easy
+
+public class Solution {
+    public int[] PlusOne(int[] digits) {
+        string n = string.Join("", digits);
+        n = (BigInteger.Parse(n) + 1).ToString();
+        int[] result = new int[n.Length];
+        for (int i = 0; i < n.Length; i++)
+        {
+            result[i] = n[i] - '0';
+        }
+        return result;
+    }
+}
+        
