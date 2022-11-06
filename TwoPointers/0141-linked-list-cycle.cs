4c6ccaab46a41dd61f9282e676edfd266424013b
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2022-11-06 09:24:40 +0100

    Create 0141-linked-list-cycle.cs

---
+++ TwoPointers/0141-linked-list-cycle.cs
@@ -0,0 +1,32 @@
+
+// 141. Linked List Cycle
+// https://leetcode.com/problems/linked-list-cycle/
+// Difficulty: Easy
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public bool HasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow.Equals(fast)) return true;
+        }
+        return false;
+    }
+}
+
+        
