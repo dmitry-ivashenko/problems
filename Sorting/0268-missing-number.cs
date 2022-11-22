97f652009fc48d70e980563d44bdbad67a0768c1
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2022-11-22 11:44:20 +0100

    Create 0268-missing-number.cs

---
+++ Sorting/0268-missing-number.cs
@@ -0,0 +1,16 @@
+
+// 268. Missing Number
+// https://leetcode.com/problems/missing-number/
+// Difficulty: Easy
+
+public class Solution {
+    public int MissingNumber(int[] nums) {
+        int sum = 0;
+        int total = nums.Length * (nums.Length + 1) / 2;
+        for (int i = 0; i < nums.Length; i++) {
+            sum += nums[i];
+        }
+        return total - sum;
+    }
+}
+        
