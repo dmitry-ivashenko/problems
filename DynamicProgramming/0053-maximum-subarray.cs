e3999cc6a868a0e5b9f84ee99eb6f52cd759402a
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2022-12-22 14:28:54 +0100

    Create 0053-maximum-subarray.cs

---
+++ DynamicProgramming/0053-maximum-subarray.cs
@@ -0,0 +1,22 @@
+
+// 53. Maximum Subarray
+// https://leetcode.com/problems/maximum-subarray/
+// Difficulty: Medium
+
+public class Solution {
+    public int MaxSubArray(int[] nums) {
+        int maxSub = nums[0];
+        int curSum = 0;
+        
+        for(int i = 0; i < nums.Length; i++){
+            if(curSum < 0){
+                curSum = 0;
+            }
+            curSum += nums[i];
+            maxSub = Math.Max(maxSub, curSum);            
+        }
+        return maxSub;
+    }
+}
+
+        
